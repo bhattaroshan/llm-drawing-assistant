7042bb9e8fae06403a0a948868e8c4dee98cd1b5
Roshan Bhatta <bhattaroshan@gmail.com>
2025-12-16 00:38:45 +0545

    removed .next

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GMntrmcRE2KEviiOshgWQX74XZ6XIDpNqhuJisinjc0=\"\n}"
--- frontend/.next/dev/static/chunks/app_page_tsx_607b5035._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_643fe4c2._.js",
-  "static/chunks/_12b69279._.js"
-],
-    source: "dynamic"
-});
